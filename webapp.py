11a298b18cea30ffec24f0031d0194a77cd9b9f2
Josef0801 <92641737+Josef0801@users.noreply.github.com>
2023-09-08 23:07:48 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -18,7 +18,7 @@ if 'dataframes' not in st.session_state:
     aws_secret_access_key = st.secrets["aws_secret_access_key"]
     # aws_access_key_id = "AKIAWDTQFMVEMYWGYRFJ"
     # aws_secret_access_key = "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"
-    # aws_session_token = None  # Optional, use only if you are using temporary credentials
+    aws_session_token = None  # Optional, use only if you are using temporary credentials
 
     # Initialize a session using Amazon S3
     session = boto3.Session(
