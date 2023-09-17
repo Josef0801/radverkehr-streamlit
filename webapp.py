1298212f9e41b063ba7c227268ed9998dffede16
Josef0801 <92641737+Josef0801@users.noreply.github.com>
2023-09-17 23:43:45 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -68,10 +68,10 @@ selected_dfs = st.sidebar.multiselect('Select the DataFrames', options=list(data
 start_date = st.sidebar.date_input('Start date', value=pd.to_datetime('2023-06-20').date())
 end_date = st.sidebar.date_input('End date', value=pd.to_datetime('2023-09-07').date())
 num_days = (end_date - start_date).days
-print("NUM DAYS")
-print(num_days)
+
 
 popularity_data = []
+popularity_data_hour = pd.DataFrame(columns=["lat", "long", "popularity", "hour"])
 for key in dataframes:
     df = dataframes[key]
 
@@ -80,17 +80,115 @@ for key in dataframes:
     popularity = sum(df["bikes_joined"])
     popularity += (-1 * sum(df.bikes_left))
     popularity_data.append((st.session_state.stations_lat[key], st.session_state.stations_long[key], popularity))
-       
+    
+    df["day"] = df["date_time"].dt.date
+    df["hour"] = df["date_time"].dt.hour
 
+    summed_df = df.groupby("hour").sum().reset_index()
+
+    summed_df["joined_average"] = summed_df["bikes_joined"] /81
+    summed_df["left_average"] = summed_df["bikes_left"] /81
+    summed_df["popularity"] = summed_df["joined_average"] - 1 * summed_df["left_average"] 
+    summed_df["lat"] = st.session_state.stations_lat[key]
+    summed_df["long"] = st.session_state.stations_long[key]
+    
+    popularity_data_hour = pd.concat([popularity_data_hour, summed_df[["lat", "long", "popularity", "hour"]]])
+    
+    
 # Convert to DataFrame for easier processing
+
 popularity_df = pd.DataFrame(popularity_data, columns=["lat", "lon", "popularity"])
+
 
 # Normalize popularity for visualization
 max_popularity = popularity_df['popularity'].max()
 popularity_df['norm_popularity'] = popularity_df['popularity'] / max_popularity
 
-print("POP DF")
-print(popularity_df)
+
+# ------------------------------------ Statistics ------------------------------
+st.divider()
+st.title("Statistics")
+st.write("""
+         Here some descriptive statistics.
+         """)
+
+
+
+# Filter the data
+df = st.session_state.routes_df.copy()
+filtered_data = df[(df['date'].dt.date  >= start_date) & (df['date'].dt.date <= end_date)]
+date_diff = (end_date - start_date).days + 1
+
+col1, col2, col3, col4 = st.columns(4)
+
+col1.metric("Total number of commutes", value=len(filtered_data))
+          
+col2.metric("Average number of commutes per day", value=np.round(len(filtered_data)/date_diff, 2))
+    
+
+
+avg_duration = np.round(np.mean(filtered_data['duration']), 2)
+col3.metric("Average duration", value=avg_duration)
+
+median_duration = np.median(np.round(filtered_data['duration'], 2))
+col4.metric("Median duration", value=median_duration)
+
+st.subheader("Number of commutes")
+commutes_per_day = filtered_data.groupby(filtered_data['date'].dt.date).size()
+st.line_chart(commutes_per_day)
+
+# ------------- Hour Plot
+
+import numpy as np
+
+st.session_state.routes_df["hour"] = st.session_state.routes_df["date"].dt.hour
+
+st.subheader("Average number of commutes per hour")
+
+grouped_df = np.round(st.session_state.routes_df.groupby("hour").count()/81, 2)
+grouped_df["Avg. number of commutes"] = grouped_df["from"]
+
+
+
+st.line_chart(grouped_df["Avg. number of commutes"] )
+
+# -------------- Bar chart
+
+
+# # Filter out rows with non-official 'from' and 'to' locations
+# viable_routes = filtered_data[filtered_data['from'].isin(st.session_state.stations_lat ) & filtered_data['to'].isin(st.session_state.stations_lat )]
+
+# # Group by 'from' and 'to', then calculate average duration
+# grouped_df = viable_routes.groupby(['from', 'to']).agg({'duration': 'mean'}).reset_index()
+# grouped_df["duration"] = grouped_df["duration"].round(2) 
+
+# # Sort by duration and take top 10
+# top_routes = grouped_df.sort_values(by='duration', ascending=False).head(100)
+
+# # Streamlit app
+# st.subheader("10 longes Routes by Average Duration")
+# top_routes['label'] = top_routes['from'] + " → " + top_routes['to']
+
+# chart = (
+#     alt.Chart(top_routes)
+#     .mark_bar()
+#     .encode(
+#         y=alt.Y('label:O', title="Route", sort='-x'), # 'O' indicates ordinal data type
+#         x=alt.X('duration:Q', title="Average Duration"), # 'Q' indicates quantitative data type
+#         color=alt.Color('duration:Q', scale=alt.Scale(scheme='blueorange')), # Color by duration
+#         tooltip=['from', 'to', 'duration'] # Tooltip information on hover
+#     )
+#     .properties(width=600, height=400, title="Top 10 Routes by Average Duration")
+# )
+
+# st.altair_chart(chart, use_container_width=True)
+# # Visualize the data using a bar plot
+# # st.bar_chart(top_routes.set_index(['from', 'to'])['duration'])
+
+
+
+# ------------------------------------ Circle plot
+
 # Set Streamlit title and instructions
 st.title("Popularity of Stations")
 st.write("""
@@ -127,11 +225,78 @@ chart = pdk.Deck(
 
 st.pydeck_chart(chart)
 
+
+
+
+
+
+
+
+
+# -------------------------------- ROUTE PLOT
+import folium
+from streamlit_folium import folium_static
+
+st.title("Visualizing Popular Routes")
+st.write("Below you can see all the routes with more than 500 commutes. The red-ness and thickness indicate the popularity")
+
+df = st.session_state.routes_df
+
+df = df.groupby(['from_coordinates', 'to_coordinates', 'from', 'to']).size().reset_index(name='popularity')
+df = df[df["popularity"] >= 400]
+
+df['from_coordinates'] = df['from_coordinates'].apply(eval)
+df['to_coordinates'] = df['to_coordinates'].apply(eval)
+
+df["start_coordinates"] = df["from_coordinates"].apply(lambda x: (x[1], x[0]))
+df["target_coordinates"] = df["to_coordinates"].apply(lambda x: (x[1], x[0]))
+
+# df["color"] = df["color"].apply(hex_to_rgb)
+GREEN_RGB = [0, 255, 0, 40]
+RED_RGB = [240, 100, 0, 40]
+
+view_state = pdk.ViewState(latitude=50.848502, longitude=8.764375, zoom=12,
+    pitch=50)
+
+def calculate_color(value, max_value=1400):
+
+    value = max(0, min(max_value, value))
+    
+    red_intensity = int(255 * (value / max_value))
+    
+    return (red_intensity, 255 - red_intensity, 255 - red_intensity)
+
+df["color"] = df["popularity"].apply(lambda x: calculate_color(x))
+
+arc_layer = pdk.Layer(
+    "ArcLayer",
+    data=df,
+    get_width="popularity * 0.003",
+    get_source_position="start_coordinates",
+    get_target_position="target_coordinates",
+    get_tilt=15,
+    get_source_color="color",
+    get_target_color="color",
+    pickable=True,
+    auto_highlight=True,
+)
+
+
+chart = pdk.Deck(
+    initial_view_state=view_state,
+    layers=arc_layer,
+tooltip={"text": "From:\t{from}\nTo:\t{to}\nPopularity:\t{popularity}"}
+)
+
+st.pydeck_chart(chart)
+
+
+# ------------------------------------  Arrival and Departures ------------------------------
+
 st.divider()
-st.title("Popularity of routes")
+st.title("Arrival and Departures")
 st.write("""
-         The bigger and more intensive the circle, 
-         the more popular the location is.
+         The following plots show the departures and arrivals and the selected stations.
          """)
     
 # Iterate over the selected DataFrames
@@ -155,12 +320,12 @@ for key in selected_dfs:
     # Generate the list of timestamps at noon and midnight for each day
     noon_and_midnight = pd.date_range(df.index.min().date(), df.index.max().date() + pd.Timedelta(days=1), freq='12H')
 
-    # Add vertical lines at each timestamp in the list
-    for timestamp in noon_and_midnight:
-        if timestamp.hour == 0:  # 12 AM
-            plt.axvline(timestamp, color='darkgrey', linestyle='--', linewidth=2)
-        else:  # 12 PM
-            plt.axvline(timestamp, color='black', linestyle='--', linewidth=2)
+#     # Add vertical lines at each timestamp in the list
+#     for timestamp in noon_and_midnight:
+#         if timestamp.hour == 0:  # 12 AM
+#             plt.axvline(timestamp, color='darkgrey', linestyle='--', linewidth=2)
+#         else:  # 12 PM
+#             plt.axvline(timestamp, color='black', linestyle='--', linewidth=2)
 
     # Set the labels and title
     plt.ylim(-10, 10)
@@ -176,106 +341,3 @@ for key in selected_dfs:
 
     # Show the plot
     st.pyplot(plt)
-
-
-    
-import pandas as pd
-import streamlit as st
-import pydeck as pdk
-
-# Sample Data
-data = pd.DataFrame({
-    'from': ['BIKE 385534', 'Friedrichplatz', 'Südbahnhof Westseite', 'BIKE 385822', 'Ketzerbach/Zwischenhausen'],
-    'to': ['Universitätsstraße/Bibliothek Jura', 'Biegenstraße/Cineplex', 'Wilhelmsplatz', 'Frankfurter Straße/Theater', 'Hauptbahnhof'],
-    'date': ['2023-06-19 22:27:31.019026', '2023-06-19 22:35:49.360453', '2023-06-19 22:39:49.184764', '2023-06-19 22:43:49.184131', '2023-06-19 22:43:49.184131'],
-    'from_coordinates': ['(50.807716, 8.769865)', '(50.80327, 8.76406)', '(50.79527, 8.762049)', '(50.8034, 8.767411)', '(50.81395, 8.76616)'],
-    'to_coordinates': ['(50.807063, 8.769918)', '(50.808867, 8.77305)', '(50.804725, 8.759248)', '(50.798841, 8.762144)', '(50.819957, 8.773736)']
-})
-
-# Convert 'date' column to datetime
-data['date'] = pd.to_datetime(data['date'])
-
-# Filter by the date
-start_date = '2023-06-19'  # Update as per your need
-end_date = '2023-06-20'    # Update as per your need
-filtered_data = data[(data['date'] >= start_date) & (data['date'] <= end_date)]
-
-# Group by the from_coordinates and to_coordinates and get the popularity score
-grouped_data = filtered_data.groupby(['from_coordinates', 'to_coordinates']).size().reset_index(name='popularity')
-
-# Convert string representations to actual lists
-grouped_data['from_coordinates'] = grouped_data['from_coordinates'].apply(lambda x: list(eval(x)))
-grouped_data['to_coordinates'] = grouped_data['to_coordinates'].apply(lambda x: list(eval(x)))
-
-# Create a 'path' column
-grouped_data['path'] = grouped_data.apply(lambda row: [row['from_coordinates'], row['to_coordinates']], axis=1)
-
-# Set up pydeck chart
-view_state = pdk.ViewState(latitude=50.807894, longitude=8.766971, zoom=12, pitch=0)
-
-# Create PathLayer
-routes_layer = pdk.Layer(
-    type="PathLayer",
-    data=grouped_data,
-    get_path="path",
-    get_width="popularity * 10000",
-    get_color="[255, 100, 100, 200]",  # Adjust as per your needs
-    pickable=True,
-    width_scale=20,
-    width_min_pixels=20,
-)
-# This will return a Series of boolean values. True if the value in 'path' is a list of lists, False otherwise.
-is_list_of_lists = grouped_data['path'].apply(lambda x: isinstance(x, list) and all(isinstance(i, list) for i in x))
-
-# Print out any rows where 'path' is not a list of lists
-incorrect_format = grouped_data[~is_list_of_lists]
-st.write(incorrect_format)
-
-chart = pdk.Deck(initial_view_state=view_state, layers=[routes_layer], tooltip={"text": "Popularity: {popularity}"})
-
-st.pydeck_chart(chart)
-
-
-
-
-# # Prepare 'start_coordinates' and 'end_coordinates' columns in the format PathLayer expects
-# data['start_coordinates'] = data.apply(lambda row: [row['start_lon'], row['start_lat']], axis=1)
-# data['end_coordinates'] = data.apply(lambda row: [row['end_lon'], row['end_lat']], axis=1)
-
-# # Normalize the count column for better visual representation
-# max_count = data['count'].max()
-# min_count = data['count'].min()
-# data['normalized_count'] = 5 + (data['count'] - min_count) * 20 / (max_count - min_count)
-
-# # Create interpolated RGB colors
-# data['color_r'] = np.interp(data['normalized_count'], [0, 1], [0, 255]).astype(int)
-# data['color_g'] = np.interp(data['normalized_count'], [0, 1], [0, 0]).astype(int)
-# data['color_b'] = np.interp(data['normalized_count'], [0, 1], [255, 0]).astype(int)
-
-# # Create a pydeck Layer with the new color scheme
-# layer = pdk.Layer(
-#     "PathLayer",
-#     data,
-#     get_path='[start_coordinates, end_coordinates]',
-#     get_width='normalized_count * 50 + 2',  # Adjust the line thickness here
-#     get_color='[color_r, color_g, color_b, 150]',  # Using interpolated RGB values
-#     width_scale=2,
-#     width_min_pixels=2,
-#     pickable=True,
-#     auto_highlight=True
-# )
-
-# # Set the initial viewport for the map
-# view_state = pdk.ViewState(
-#     latitude=data['start_lat'].mean(),
-#     longitude=data['start_lon'].mean(),
-#     zoom=12,
-#     min_zoom=10,
-#     max_zoom=20,
-#     pitch=20,
-#     bearing=-27.36,
-# )
-
-# # Create a pydeck Deck and display it in Streamlit
-# deck = pdk.Deck(layers=[layer], initial_view_state=view_state, map_style="mapbox://styles/mapbox/light-v9")
-# st.pydeck_chart(deck)
